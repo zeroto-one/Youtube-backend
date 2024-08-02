f6ef7b83b796bc42d7a11244194a5ee8a5d6f7af
Dipendra <dipendra.12018314@lpu.in>
2024-08-02 12:45:56 +0530

    Updated user routes to include additional endpoints and middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import {loginUser,logoutUser,refreshAccessToken,registerUser,} from "../controllers/user.controller.js"; //always use .js at the end
+import {
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    registerUser,
+    updateUserAvatar,
+    updateUserCoverImage,
+    updateUserDetails,
+} from "../controllers/user.controller.js"; //always use .js at the end
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js"; //this
 
@@ -7,24 +19,35 @@ export const router = Router();
 
 //now use this Router when /user pe call ani control idr aa jana then /user/register pe post req ani then re registerUser function call ho jana
 router.route("/register").post(
-  upload.fields(
-    // * here we wrote middleware to handle our files
-    //  you can add multiple files here like this {name:"file1", maxCount:1}, {name:"file2", maxCount:1} etc
-    [
-      {
-        name: "avatar",
-        maxCount: 1,
-      },
-      {
-        name: "coverImage",
-        maxCount: 1,
-      },
-    ] // this filed is a array  of files which we want to upload
-  ),
-  registerUser
+    upload.fields(
+        // * here we wrote middleware to handle our files
+        //  you can add multiple files here like this {name:"file1", maxCount:1}, {name:"file2", maxCount:1} etc
+        [
+            {
+                name: "avatar",
+                maxCount: 1,
+            },
+            {
+                name: "coverImage",
+                maxCount: 1,
+            },
+        ] // this filed is a array  of files which we want to upload
+    ),
+    registerUser
 ); //explain
 
 router.route("/login").post(loginUser);
 //login ke bad wale routes AKA secure Routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-details").patch(verifyJWT, updateUserDetails);
+router
+    .route("/update-avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+    .route("/update-cover-image")
+    .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
